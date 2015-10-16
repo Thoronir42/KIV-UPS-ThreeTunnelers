e27e15c394950e6237d9f3627a02a626864ac548
Thoronir42 <Thoronir42@Gmail.com>
2015-10-16 22:37:36 +0200

    Mockup server sophistication, threading, basic player-name checking.

--- src/generic/BackServer.java
+++ src/generic/BackServer.java
@@ -2,41 +2,75 @@ package generic;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 //	-Djava.net.preferIPv4Stack=true
-public abstract class BackServer
+public class BackServer extends Thread
 {
-    public static void main( String[] args )
-    {
-        int bufferSize = 60; 
-        DatagramSocket ds;
-        try
-        {
+    private static final int BUFFER_SIZE = 60; 
+    
+    DatagramSocket ds;    
+    
+    private DatagramPacket lastReceived;
+    
+    public boolean cont;
+    
+    @Override
+    public void run(){
+        try {
             ds = new DatagramSocket( tunnelers.structure.Settings.DEFAULT_PORT );
-            while (true)
-            {
-                byte[] buffer = new byte[bufferSize];
-                System.out.print( "Server ceka...\n");
-                DatagramPacket recv = new DatagramPacket(buffer, buffer.length);
-                ds.receive(recv);
+            
+            cont = true;
+            while (cont) {
+                System.out.format("Serverrr (%s:%d) ceka...%n", ds.getLocalSocketAddress(), ds.getLocalPort());
                 
-                InetAddress adresa = recv.getAddress();
-                int port = recv.getPort();
+                String data = receiveMessage();
+                System.out.println( ">> "+data);
                 
-                System.out.format("Pripojil se klient z %s:%d(%s)%n", adresa.getHostAddress(), port, adresa.getHostName());
-                
-                String data = new String( buffer );
-                System.out.print( "Prijal sem data - "+data+"\n");
-                data = "SRV:"+data;
-                buffer = data.getBytes();
-                
-                
-                System.out.print("Odesilam = "+data+" o velikosti "+buffer.length+" \n");
-                DatagramPacket sendX = new DatagramPacket( buffer, buffer.length, adresa, port);
-                ds.send( sendX );
+                if(!data.contains(":")){
+                    data = "NA:"+data;
+                }
+                sendMessage(data);
             }
         } catch (Exception e) {
-            System.err.println(e);
+            e.printStackTrace();
         }
+    }
+    
+    private String receiveMessage() throws IOException{
+        byte[] buffer = new byte[BUFFER_SIZE];
+        lastReceived = new DatagramPacket(buffer, buffer.length);
+        ds.receive(lastReceived);
+        
+        InetAddress adresa = lastReceived.getAddress();
+        int port = lastReceived.getPort();
+
+        System.out.format("> Klient %s:%d(%s)%n", adresa.getHostAddress(), port, adresa.getHostName());
+        return new String( buffer );
+    }
+    synchronized public void sendMessage(String text) throws IOException{
+        byte[] buffer = text.getBytes();
+        System.out.print("Odesilam = "+text+" o velikosti "+buffer.length+" \n");
+        
+        InetAddress adresa = lastReceived.getAddress();
+        int port = lastReceived.getPort();
+        
+        DatagramPacket sendX = new DatagramPacket( buffer, buffer.length, adresa, port);
+        ds.send( sendX );
+    }
+    
+    public static void main( String[] args ) throws InterruptedException, IOException
+    {
+        BackServer server = new BackServer();
+        server.start();
+        Scanner sc = new Scanner(System.in);
+        String text;
+        while((text = sc.nextLine())!= "exit"){
+            server.sendMessage("SRV:" + text);
+        }
+        server.cont = false;
+        server.join();
+        
+        
     }
 }
