773cf710927873fb2453dab6d45dba71f715d5e7
Thoronir42 <Thoronir42@Gmail.com>
2017-01-25 20:16:35 +0100

    Protocol command type remap

--- src/tunnelers/network/command/CommandType.java
+++ src/tunnelers/network/command/CommandType.java
@@ -1,47 +1,45 @@
 package tunnelers.network.command;
 
 public enum CommandType {
-	Undefined(0),
+	Undefined(0x00),
 	//    LEAD
-	LeadIntroduce(1),
-	LeadDisconnect(2),
-	LeadMarco(3),
-	LeadPolo(4),
-	LeadBadFormat(5),
-	//    CLIENT
-	ClientSetName(10),
+	LeadIntroduce(0x01),
+	LeadDisconnect(0x02),
+	ClientSetName(0x03),
+	LeadMarco(0x04),
+	LeadPolo(0x05),
 	
-	RoomsList(14),
-	RoomsCreate(16),
-	RoomsJoin(17),
-	RoomsLeave(18),
+	RoomsList(0x10),
+	RoomsCreate(0x12),
+	RoomsJoin(0x13),
+	RoomsLeave(0x14),
 	//    MESSAGE
-	MsgPlain(40),
-	MsgSystem(41),
-	MsgRcon(42),
+	MsgPlain(0x20),
+	MsgSystem(0x21),
+	MsgRcon(0x22),
 	//    ROOM CONTROLS
-	RoomSyncState(60),
-	RoomReadyState(61),
+	RoomSyncState(0x40),
+	RoomReadyState(0x41),
 	//    CLIENT RELATED COMMANDS
-	RoomClientInfo(65),
-	RoomClientLatency(66),
-	RoomClientRemove(67),
-	RoomSetLeader(68),
+	RoomClientInfo(0x42),
+	RoomClientLatency(0x43),
+	RoomClientRemove(0x44),
+	RoomSetLeader(0x45),
 	//    PLAYER CONTROLLING COMMANDS
-	RoomPlayerAttach(80),
-	RoomPlayerDetach(81),
-	RoomPlayerMove(82),
-	RoomPlayerSetColor(83),
+	RoomPlayerAttach(0x50),
+	RoomPlayerDetach(0x51),
+	RoomPlayerMove(0x52),
+	RoomPlayerSetColor(0x53),
 	//    MAP COMMANDS
-	MapSpecification(90),
-	MapBases(91),
-	MapChunkData(92),
-	MapBlocksChanges(93),
+	MapSpecification(0x60),
+	MapBases(0x61),
+	MapChunkData(0x62),
+	MapBlocksChanges(0x63),
 	//    GAME-ENTITY CONTROLS
-	GameControlsSet(120),
-	GameTankInfo(130),
-	GameProjAdd(140),
-	GameProjRem(141),;
+	GameControlsSet(0x70),
+	GameTankInfo(0x74),
+	GameProjAdd(0x7A),
+	GameProjRem(0x7B),;
 
 	private final short value;
 
